54a6eed0e21dcbc4dcecf66c0be849b3b8dc664c
Steven <steven.camillieri@protonmail.com>
2022-01-06 09:57:03 -0500

    added object to dice and called nanoid for key

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/header/Header";
 import Die from "./components/die/Die";
 import Button from "./components/button/Button";
-
+import { nanoid } from "nanoid";
 import "./App.css";
 
 function App() {
@@ -11,20 +11,26 @@ function App() {
 
   // generates an array of 10 random numbers
   function allNewDice() {
-    const numArray = [];
+    let numObject = {};
+    let numArray = [];
     for (let i = 0; i < 10; i++) {
-      let x = Math.ceil(Math.random() * 6);
-      numArray.push(x);
+      numObject = {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      };
+      numArray.push(numObject);
     }
     return numArray;
   }
 
+  // roll the dice
   function rollDice() {
     setDice(allNewDice());
   }
-
+console.log(dice)
   // This creates the 10 dice componenets c random nums
-  const newDice = dice.map((i) => <Die value={i} />);
+  const newDice = dice.map((i) => <Die key={i.id} value={i.value} isHeld={i.isHeld} />);
 
   return (
     <div id="page">
